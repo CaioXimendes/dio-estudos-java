aa416bb011c9e1da8a66d1ccd9c54435787086a8
CaioXimendes <caioximendes1@gmail.com>
2023-09-02 17:56:51 -0300

    Estudo sobre condição ternaria no java

---
+++ estruturas_condicionais/src/exemplos/CondicaoTernaria.java
@@ -0,0 +1,13 @@
+package exemplos;
+
+public class CondicaoTernaria 
+{
+	public static void main(String[] args) 
+	{
+		int a = 5;
+		int b = 7;
+		
+		String comparacao = a > b ?"Verdadeiro":"Falso";
+		System.out.println(comparacao);
+	}
+}
